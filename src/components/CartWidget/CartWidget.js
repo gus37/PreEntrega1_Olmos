00c96d15c60bd54bea35a3840543cd263467aa1f
Gustavo olmos <grolmos159@gmail.com>
2023-09-10 23:59:12 -0300

    add css to NavBar

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -3,9 +3,9 @@ import cart from './assets/mbrishoppingcart_99558.svg';
 //lo importo en el navbar pq quiero que este dentro de el.
 const CartWidget = () => {
     return (
-        <div>
-            <img className='mx-5' src={cart} alt='cart-widget'/>
-            0
+        <div className='is-flex is-justify-content-center'>
+            <img className='mx-5 mb-2' src={cart} alt='cart-widget'/>
+            <p className='mt-2'>0</p>
         </div>
     )
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,19 @@
 import CartWidget from "../CartWidget/CartWidget";
 import logo from './assets/logo.png'
-
+import './NavBar.css'
 
 const NavBar = () => {
     return (
-        <nav className="has-background-primary">
-            
-            <img src={logo} alt="logo"/>  
-           
-            <h3 className="title">Eco-commerce</h3>
+        <nav className="has-background-primary">           
+
+            <img className='logo_img' src={logo} alt="logo" />             
+
+            <h3 className="logo_text">Eco-Moda</h3>
 
             <div>
-                <button className="button m-2 is-medium is-responsive is-dark is-outlined is-rounded">Celulares</button>
-                <button className="button m-2 is-medium is-responsive is-dark is-outlined is-rounded">Tablets</button>
-                <button className="button m-2 is-medium is-responsive is-dark is-outlined is-rounded">NoteBooks</button>
+                <button className="btn button m-2 is-large is-responsive is-dark is-outlined is-rounded">Hombres</button>
+                <button className="btn button m-2 is-large is-responsive is-dark is-outlined is-rounded">Mujeres</button>
+                <button className="btn button m-2 is-large is-responsive is-dark is-outlined is-rounded">Niños</button>
             </div>
 
             <CartWidget/>
